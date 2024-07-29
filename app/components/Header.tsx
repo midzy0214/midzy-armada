97772d9c85a2d7b7a9378c646d9a9c53cb26d639
Johan Sangalang <jnsangalang93@yahoo.com>
2024-07-29 15:57:01 -0700

    added animation on hover and click to buttons in Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Lexend_Tera, Kameron } from "next/font/google";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export const lexendTera = Lexend_Tera({
   subsets: ["latin"],
@@ -25,36 +26,96 @@ export function Header() {
         Freelance Makeup Artist
       </h1>
       <div className="w-full flex flex-wrap mb-2">
-        <button
+        <motion.button
+          initial={{
+            background: "radial-gradient( rgb(255,255,255), rgb(224,158,143))",
+          }}
+          whileHover={{
+            background: "radial-gradient( rgb(224,158,143), rgb(255,255,255)) ",
+          }}
+          whileTap={{
+            background: "radial-gradient( rgb(224,158,143), rgb(0,0,0))",
+          }}
+          transition={{ duration: 0.6 }}
           className={`w-1/3 lg:w-1/6  border-2 border-green-200 mb-1 text-sm font-semibold lg:text-3xl  ${kameron.className}`}
         >
           <Link href={"/"}>Welcome</Link>
-        </button>
-        <button
+        </motion.button>
+        <motion.button
+          initial={{
+            background: "radial-gradient( rgb(255,255,255), rgb(224,158,143))",
+          }}
+          whileHover={{
+            background: "radial-gradient( rgb(224,158,143), rgb(255,255,255)) ",
+          }}
+          whileTap={{
+            background: "radial-gradient( rgb(224,158,143), rgb(0,0,0))",
+          }}
+          transition={{ duration: 0.6 }}
           className={`w-1/3 lg:w-1/6  border-2 border-green-200 mb-1 text-sm font-semibold lg:text-3xl ${kameron.className}`}
         >
           <Link href={"about"}>About</Link>
-        </button>
-        <button
+        </motion.button>
+        <motion.button
+          initial={{
+            background: "radial-gradient( rgb(255,255,255), rgb(224,158,143))",
+          }}
+          whileHover={{
+            background: "radial-gradient( rgb(224,158,143), rgb(255,255,255)) ",
+          }}
+          whileTap={{
+            background: "radial-gradient( rgb(224,158,143), rgb(0,0,0))",
+          }}
+          transition={{ duration: 0.6 }}
           className={`w-1/3 lg:w-1/6  border-2 border-green-200 mb-1 text-sm font-semibold lg:text-3xl ${kameron.className}`}
         >
           Services
-        </button>
-        <button
+        </motion.button>
+        <motion.button
+          initial={{
+            background: "radial-gradient( rgb(255,255,255), rgb(224,158,143))",
+          }}
+          whileHover={{
+            background: "radial-gradient( rgb(224,158,143), rgb(255,255,255)) ",
+          }}
+          whileTap={{
+            background: "radial-gradient( rgb(224,158,143), rgb(0,0,0))",
+          }}
+          transition={{ duration: 0.6 }}
           className={`w-1/3 lg:w-1/6  border-2 border-green-200 mb-1 text-sm font-semibold lg:text-3xl ${kameron.className}`}
         >
           <Link href={"contact"}>Contact</Link>
-        </button>
-        <button
+        </motion.button>
+        <motion.button
+          initial={{
+            background: "radial-gradient( rgb(255,255,255), rgb(224,158,143))",
+          }}
+          whileHover={{
+            background: "radial-gradient( rgb(224,158,143), rgb(255,255,255)) ",
+          }}
+          whileTap={{
+            background: "radial-gradient( rgb(224,158,143), rgb(0,0,0))",
+          }}
+          transition={{ duration: 0.6 }}
           className={`w-1/3 lg:w-1/6  border-2 border-green-200 mb-1 text-sm font-semibold lg:text-3xl ${kameron.className}`}
         >
           Portfolio
-        </button>
-        <button
+        </motion.button>
+        <motion.button
+          initial={{
+            background: "radial-gradient( rgb(255,255,255), rgb(224,158,143))",
+          }}
+          whileHover={{
+            background: "radial-gradient( rgb(224,158,143), rgb(255,255,255)) ",
+          }}
+          whileTap={{
+            background: "radial-gradient( rgb(224,158,143), rgb(0,0,0))",
+          }}
+          transition={{ duration: 0.6 }}
           className={`w-1/3 lg:w-1/6  border-2 border-green-200 mb-1 text-sm font-semibold lg:text-3xl ${kameron.className}`}
         >
           Reviews
-        </button>
+        </motion.button>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,8 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        homeButtons:
+          "linear-gradient(120deg, rgb(224,158,143), rgb(255,255,255))",
       },
       colors: {
         backgroundGray: "rgb(211,208,203)",
